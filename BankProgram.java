b3169ca569ad6e0f646b0052a54d8587bb43a97b
Vaishnavi-Patil860 <vaishnavipatil3443@gmail.com>
2025-05-01 23:17:22 +0530

    JAVA APPLICATIONS

---
+++ BankProgram.java
@@ -0,0 +1,254 @@
+import java.util.*;
+class BankProgram
+{
+    static String name;
+    static String email;
+    static String address;
+    static double bal;
+    static int pin;
+    static String contact;
+    static Long adhar;
+    static String pancard;
+    static Long accountNumber;
+    static String ifscCode;
+    static ArrayList<String>  statement = new ArrayList<String>();
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        welcomeLoop:
+        for (; ;)
+        {
+            System.out.println();
+            System.out.println("     WELCOME");
+            System.out.println("    LAXMI CHIT FUND       ");
+            System.out.println();
+            System.out.println("     1. Create Account    ");
+            System.out.println("     2. Existing User     ");
+            System.out.println();
+            System.out.print("     Enter your option :     ");
+
+            switch (sc.nextInt())
+             {
+                case 1:{
+
+                          System.out.println();
+                          System.out.print("   create your account  ");
+                          System.out.println();
+
+                          sc.nextLine();
+                          System.out.println();
+                          System.out.print("    username :");
+                          name = sc.nextLine();
+                          System.out.println();
+                          System.out.print("    email :  ");
+                          email = sc.next();
+                          System.out.println();
+                          System.out.print("    Address : ");
+                          sc.nextInt();
+                          System.out.println();
+
+                          address = sc.nextLine();
+                          System.out.print("    Adhar Number:  ");
+
+                          adhar = sc.nextLong();
+                          System.out.println();
+
+                          System.out.print("    Pan Number: ");
+                          pancard = sc.next();
+                          System.out.println();
+
+                          System.out.print("    Contact Number:  ");
+                          contact = sc.next();
+                          System.out.println();
+
+                          System.out.print("     Pin:   ");
+                          pin = sc.nextInt();
+                          System.out.println();
+
+                          System.out.print("     Enter an ammount (Deposit)  : ");
+                          bal = sc.nextDouble();
+                          System.out.println();
+
+                          statement.add("  Deposit : "  + bal + "rs   (Account Creation)");
+                          System.out.println();
+                          System.out.println("  Account created successfully");
+                          System.out.println();
+                          break;
+
+                    }
+
+                case 2 :{
+                          if (name == null)
+                          {
+                            System.out.println();
+                            System.out.println("  Create your account first ");
+                            break;
+                          }
+                          System.out.println();
+                          System.out.println("    LOGIN ");
+                          for (int i = 3; i >= 1; i--)
+                          {
+                            System.out.println();
+                            System.out.print("   username/Phone:");
+                            String cred1 = new Scanner(System.in).next();
+                            System.out.print("   Pin : " );
+
+
+                            int cred2 = new Scanner(System.in).nextInt();
+                            System.out.println();
+                            if((cred1.equals(name)||cred1.equals(contact)) && cred2 == pin){
+
+                              homePage:
+                              for (;;)
+                              {
+                                System.out.println();
+                                System.out.println("home page");
+                                System.out.println();
+                                System.out.println("1. deposit ammount");
+                                System.out.println("2. withdraw ammount");
+                                System.out.println("3. check balance ");
+                                System.out.println("4. statement");
+                                System.out.println("5. loan");
+                                System.out.println("6. profile");
+                                System.out.println("7. logout");
+                                System.out.println();
+                                System.out.print("   enter your option :");
+                                int opt = new Scanner(System.in).nextInt();
+                                System.out.println();
+
+                         switch(opt)
+                         {
+                             case 1 :{
+                                   System.out.print("   deposit ammount :");
+                                   System.out.println();
+                                   System.out.print("   enter the ammount: ");
+                                   double depAmt = new Scanner(System.in).nextDouble();
+                                   bal += depAmt;
+                                   System.out.print("   amount deposited");
+                                   statement.add("deposit :"  + depAmt + "rs");
+                                   break;
+                                  }
+                              case 2 :
+                              {
+                                   System.out.print("withdraw module");
+                                   System.out.println();
+                                   System.out.print("ENTER YOUR AMMOUNT :");
+                                   double withAmt = new Scanner(System.in).nextDouble();
+                                   System.out.print("ENTER THE PIN: ");
+                                   int pin1 = new Scanner(System.in).nextInt();
+                                       if(pin1 == pin){
+                                          if(withAmt <= bal)
+                                            {
+                                              bal -= withAmt;
+                                              System.out.print("ACCOUNT DEBITED SUCCESSFULLY");
+                                              statement.add("debited :" + withAmt + "rs");
+                                            }
+                                         else 
+                                           {
+                                             System.out.println("");
+                                            }
+                                          }
+                                        else {
+                                            System.out.println("invalid pin");
+                                             }
+                                             break;
+                                            }
+
+                                  
+                                
+                              case 3:{
+                                  System.out.println("CHECK BALANCE MODULE");
+                                  for(int j= 3; j>= 1; j--)
+                                  {
+                                  System.out.println();
+                                  System.out.print("ENTER YOUR PIN");
+                                  int pin1 = new Scanner(System.in).nextInt();
+                                  if(pin == pin1){
+                                   System.out.print("YOUR ACCOUNT BALANCE IS :" + bal);
+                                   continue homePage;
+
+
+                                   }
+                                   else {
+                                  System.out.println("INVALID PIN ");
+                                  System.out.print("ATTEMPT LEFT :" + (j-1));
+
+                                    }
+
+                                  }
+
+                                    System.out.println("ITS NOT YOUR ACCOUNT");
+                                    System.out.println("ACCOUNT IS BLOCKED FOR 24 HOURS");
+                                    System.exit(0);
+                                    break;
+                                  }
+                             case 4 :{
+                                    System.out.println("STATEMENT MODULE");
+                                   System.out.println();
+                                   for (String s : statement ){
+                                   System.out.println(s);
+
+                                   }
+                                    break;
+                                }
+
+                            case 5 :{
+                                     System.out.println("LOAN MODULE");
+                                     break;
+                                    }
+                            
+                            case 6 :{
+                                     System.out.println(" PROFILE MODULE");
+                                     System.out.print("USERNAME :" + name);
+                                     System.out.print("USERNAME :" + contact);
+                                     System.out.print("USERNAME :" + email);
+                                     System.out.print("USERNAME :" + address);
+
+
+
+                                     break;
+                                      }
+                            case 7 :{
+                                     System.out.println("THANKYOU VISIT AGAIN");
+                                     System.out.println();
+                                     System.exit(0);
+                                     break;
+                                      }
+
+                            default:{
+                                      System.out.println("INVALID OPTION");
+                                      break;
+                      
+                                     }
+                                   }
+                                 }
+                               }
+                        else {
+                            System.out.println("INVALID CRED");
+                            System.out.println("ATTEMPT LEFT : " + (i-1));
+
+                          }
+                        }
+        
+                           System.out.println("THANKYOU AND NAVAR VISIT AGAIN");
+                           System.out.println("YOUR ACCOUNT IS DEACTIVATED FOR 48 HOURS");
+                           System.exit(0);
+                        }
+
+                        default :{
+                            System.out.println("INVALID OPTION");
+                            break;
+
+                          }
+                       }
+                    }
+                 }
+              }  
+        
+      
+
+
+
+
+
+
---
+++ PasswordProgram.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PasswordProgram {
+    public static void main(String[] args)  throws InterruptedException {
+        
+        Scanner sc = new Scanner(System.in);
+        int storedPin = 1234;
+        int duration = 5000;
+        outerLoop:
+        for(;;)
+        {
+            int attempt = 3;
+            do{
+                System.out.println();
+                System.out.println("Enter your pin : ");
+                int userPin = sc.nextInt();
+                if(userPin == storedPin)
+                {
+                    System.out.println("Phone Unlocked");
+                    break outerLoop;
+                }
+                
+                else {
+                    System.out.println("WRONG PIN ENTERED : ");
+                    System.out.println("attempt left :" +(attempt-1));
+                }
+                attempt--;
+
+            }
+            while (attempt>=1);
+            System.out.println();
+            System.out.println("Phone is been locked for : " + (duration/1000)+ "  seconds");
+            Thread.sleep(duration);
+            duration *= 2;
+
+        }
+    }
+}
